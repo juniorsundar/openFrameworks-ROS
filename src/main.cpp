c955e1094563e4a7a9036d0b8a1389b4b989d42f
Junior Sundar <juniorsundar@gmail.com>
2022-11-27 14:34:38 +0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include "ofApp.h"
 
 //========================================================================
-int main( ){
+int main(int argc, char **argv){
+    // Initialise ROS node
+    ros::init(argc, argv, "ofNode");
+    
     ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 
     // this kicks off the running of my app
     // can be OF_WINDOW or OF_FULLSCREEN
     // pass in width and height too:
-    ofRunApp(new ofApp());
+    // Can pass args from node initialisation as well
+    ofRunApp(new ofApp(argc, argv));
 
 }
